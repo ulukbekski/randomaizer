0703e8f4f320cfb76f209d0b5b72ab4aa6d1e0a8
ulukbekski <88556249+ulukbekski@users.noreply.github.com>
2022-10-18 18:36:17 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,51 @@
 let ol = document.querySelector('#MyList');
 let li = ol.getElementsByTagName('li');
+
+for (let index = 0; index < li.length; index++) {
+    const element = li[index];  
+}
 function giveRandom() {
     let actives = []
     for (let index = 0; index < li.length; index++) {
         const element = li[index];
-        if (!element.getAttribute('aria-disabled')) {
-          actives.push(element)
-        }
+        if (element.getAttribute('aria-disabled') === 'false' ||
+         element.getAttribute('aria-disabled') === null) {
+        actives.push(element)
     }
-    document.querySelector('#giveRandomResult').innerHTML = actives[Math.floor(actives.length * Math.random()) ].innerText;  
+    }
+   document.querySelector('#giveRandomResult').innerHTML = 
+   actives[Math.floor(actives.length * Math.random()) ].innerText;  
 }
+
+function giveRandom2() {
+    let actives = []
+    for (let index = 0; index < li.length; index++) {
+        const element = li[index]; 
+        if (element.getAttribute('aria-disabled') === 'false' ||
+         element.getAttribute('aria-disabled') === null) {
+        actives.push(element)
+    }
+    }
+    let first =  actives[Math.floor(actives.length * Math.random()) ].innerText
+    let second = actives[Math.floor(actives.length * Math.random()) ].innerText
+
+    while(first === second){
+        second = actives[Math.floor(actives.length * Math.random()) ].innerText
+    }
+   document.querySelector('#giveRandom2Result').innerHTML = first + ' ' + second;
+   
+    
+}
+
 
 for (let index = 0; index < li.length; index++) {
     const element = li[index];
     element.addEventListener('click', function() {
-    if (!element.getAttribute('aria-disabled')) {
+    if (element.getAttribute('aria-disabled') === 'false' || element.getAttribute('aria-disabled') === null) {
         element.setAttribute("aria-disabled","true")
     }else{
         element.setAttribute("aria-disabled","false")
     }
         element.classList.toggle('disabled')
-    })
-
-    
+    }) 
 }
